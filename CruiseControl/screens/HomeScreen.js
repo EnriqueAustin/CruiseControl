2d96a82616fa23c35b7eb75ea25b658da06044af
Enrique Austin <34680513+EnriqueAustin@users.noreply.github.com>
2023-02-01 17:34:34 +0200

    ...

--- CruiseControl/screens/HomeScreen.js
+++ CruiseControl/screens/HomeScreen.js
@@ -3,8 +3,14 @@ import { StyleSheet, Text, View, Image } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`} >
       <View style={tw`p-5`}>
@@ -13,10 +19,42 @@ const HomeScreen = () => {
                 width: 350, height: 100, resizeMode: 'contain'
             }}
             source={
+                
                 // {uri: "https://links.papareact.com/gzs"}
                 // Redo logo at later stage!!
+
                 require('../assets/images/cruizecontrollogo.png')
             }
+        />
+
+        <GooglePlacesAutocomplete
+          placeholder='Where from?'
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {
+            dispatch(setOrigin({
+              location: details.geometry.location,
+              description: data.description,
+            }))
+
+            dispatch(setDestination(null))
+          }}
+          fetchDetails={true}
+          returnKeyType={'search'}
+          enablePoweredByContainer={false}
+          minLength={2}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en',
+          }}
+          nearbyPlacesAPI='GooglePlacesSearch'
+          debounce={400}
         />
 
         <NavOptions />
